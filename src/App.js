f1410628757c368fcbfd977619903b112b959451
Johan Cañas <jxexcxo@gmail.com>
2020-10-27 18:55:51 -0500

    components part 1

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { Navbar, NavbarBrand } from 'reactstrap';
 import './App.css';
+import Menu from './components/MenuComponent';
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
           </NavbarBrand>
         </div>
       </Navbar>
+      <Menu />
     </div>
   );
 }
