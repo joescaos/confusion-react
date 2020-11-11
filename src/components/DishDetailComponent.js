3e2828f6101fa76b7eca2529f001b4850e957a01
Johan Cañas <jxexcxo@gmail.com>
2020-11-11 10:24:18 -0500

    redux actions

--- src/components/DishDetailComponent.js
+++ src/components/DishDetailComponent.js
@@ -41,7 +41,7 @@ class CommentForm extends Component {
   handleSubmit(values) {
     console.log("Current state is: " + JSON.stringify(values));
     this.toggleModal();
-    alert("Current state is" + JSON.stringify(values));
+    this.props.addComment(this.props.dishId, values.rating, values.name, values.comment)
     //event.preventDefault();
   }
   render() {
@@ -138,7 +138,7 @@ function RenderDish({ dish }) {
   );
 }
 
-function RenderComments({ comments }) {
+function RenderComments({ comments, addComment, dishId }) {
   if (comments != null)
     return (
       <div className="col-12 col-md-5 m-1">
@@ -160,7 +160,7 @@ function RenderComments({ comments }) {
             );
           })}
         </ul>
-        <CommentForm />
+        <CommentForm dishId={dishId} addComment={addComment}/>
       </div>
     );
   else return <div></div>;
@@ -179,7 +179,9 @@ const DishDetail = (props) => {
           </Breadcrumb>
           <div className="row">
             <RenderDish dish={props.dish} />
-            <RenderComments comments={props.comments} />
+            <RenderComments comments={props.comments} 
+              addComment={props.addComment}
+              dishId={props.dish.id}/>
           </div>
         </div>
       </div>
--- src/components/MainComponent.js
+++ src/components/MainComponent.js
@@ -8,6 +8,7 @@ import {Switch, Route, Redirect, withRouter } from 'react-router-dom';
 import Header from './HeaderComponent';
 import Footer from './FooterComponent';
 import { connect } from 'react-redux';
+import { addComment }  from '../redux/ActionCreators';
 
 const mapStateToProps = state => {
     return {
@@ -17,6 +18,10 @@ const mapStateToProps = state => {
         leaders: state.leaders
     }
 }
+
+const mapDispatchToProps = dispatch => ({
+    addComment: (dishId, rating, author, comment) => dispatch(addComment(dishId, rating, author, comment))
+});
 
 class Main extends Component{
     constructor(props){
@@ -41,7 +46,8 @@ class Main extends Component{
                     dish={this.props.dishes.filter((dish) => dish.id === 
                         parseInt(match.params.dishId, 10))[0]}
                     comments={this.props.comments.filter((comment) => comment.dishId === 
-                        parseInt(match.params.dishId, 10))} />
+                        parseInt(match.params.dishId, 10))}
+                        addComment={this.props.addComment} />
             )
         }
         return(
@@ -61,4 +67,4 @@ class Main extends Component{
     }
 }
 
-export default withRouter(connect(mapStateToProps)(Main));
+export default withRouter(connect(mapStateToProps, mapDispatchToProps)(Main));
